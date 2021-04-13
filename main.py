701ec6d4115df52f0a1e4b9ef8ac34048e65d889
Amirmehdy Zarrinnezhad <amzarrinnezhad@gmail.com>
2021-04-14 04:15:17 +0430

    Reading dataset Added.

--- main.py
+++ main.py
@@ -1,16 +1,64 @@
-# This is a sample Python script.
+import numpy as np
+import matplotlib.pyplot as plt
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+# A function to plot images
+def show_image(img):
+    image = img.reshape((28, 28))
+    plt.imshow(image, 'gray')
+
+
+# Reading The Train Set
+train_images_file = open('train-images.idx3-ubyte', 'rb')
+train_images_file.seek(4)
+num_of_train_images = int.from_bytes(train_images_file.read(4), 'big')
+train_images_file.seek(16)
+#
+train_labels_file = open('train-labels.idx1-ubyte', 'rb')
+train_labels_file.seek(8)
+#
+train_set = []
+for n in range(num_of_train_images):
+    image = np.zeros((784, 1))
+    for i in range(784):
+        image[i, 0] = int.from_bytes(train_images_file.read(1), 'big') / 256
+
+    label_value = int.from_bytes(train_labels_file.read(1), 'big')
+    label = np.zeros((10, 1))
+    label[label_value, 0] = 1
+
+    train_set.append((image, label))
+
+# Reading The Test Set
+test_images_file = open('t10k-images.idx3-ubyte', 'rb')
+test_images_file.seek(4)
+
+test_labels_file = open('t10k-labels.idx1-ubyte', 'rb')
+test_labels_file.seek(8)
+
+num_of_test_images = int.from_bytes(test_images_file.read(4), 'big')
+test_images_file.seek(16)
+
+test_set = []
+for n in range(num_of_test_images):
+    image = np.zeros((784, 1))
+    for i in range(784):
+        image[i] = int.from_bytes(test_images_file.read(1), 'big') / 256
+
+    label_value = int.from_bytes(test_labels_file.read(1), 'big')
+    label = np.zeros((10, 1))
+    label[label_value, 0] = 1
+
+    test_set.append((image, label))
+
+# Plotting an image
+# show_image(train_set[1][0])
+# plt.show()
 
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
